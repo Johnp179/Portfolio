2dff10ffa2f179f329b49259f3dd6f5bd833e0e1
searai <johnpoconnor4@gmail.com>
2022-12-05 12:31:02 +0000

    changed how connectings to DB operate

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const path = require("path");
-require('dotenv').config();
-require("./dbconnection.js");
+require("dotenv").config();
+const connectDB = require("./dbconnection.js");
 const messageRoutes = require("./controller/message.js");
 const server = express();
 server.use(express.json());
 
-if(process.env.NODE_ENV !== "production"){
-  server.use((req, res, next)=>{
-        if(req.headers.origin) res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-        res.setHeader('Access-Control-Allow-Headers',"Content-Type");
-        res.setHeader('Access-Control-Allow-Methods',"GET, PUT, DELETE, POST");
-        res.setHeader('Access-Control-Allow-Credentials', true);
+if (process.env.NODE_ENV !== "production") {
+    server.use((req, res, next) => {
+        if (req.headers.origin)
+            res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, POST");
+        res.setHeader("Access-Control-Allow-Credentials", true);
         next();
-  })
+    });
 }
 
 server.use("/messages", messageRoutes);
-server.use("/downloads",express.static("downloads"));
+server.use("/downloads", express.static("downloads"));
 server.use("/", express.static("client/build"));
-server.get("*",(req, res)=>{
+server.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
-server.listen(port,()=>{
-    console.log(`server started on port ${port}`);
-});
 
+connectDB().then(() => {
+    server.listen(port, () => {
+        console.log("listening for requests");
+    });
+});
